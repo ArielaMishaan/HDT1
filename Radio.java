9a5a5a8a59591a4fb1c3a2bdc718012b13a2c508
ignaciomendeza <109829839+ignaciomendeza@users.noreply.github.com>
2023-01-16 11:29:45 -0600

    Métodos

--- Radio.java
+++ Radio.java
@@ -11,6 +11,12 @@ public class Radio implements IRadio{
     //constructores
 
     public Radio() {
+        frequence = "FM";
+        fMActualStation = 87.9;
+        aMActualStation = 530;
+        slotsAM = new int[12];
+        slotsFM = new double[12];
+        encendido = false;
     }
 
 
@@ -28,91 +34,117 @@ public class Radio implements IRadio{
     @Override
     public void on() {
         // TODO Auto-generated method stub
-        
+        this.encendido = true;
     }
 
     @Override
     public void off() {
         // TODO Auto-generated method stub
-        
+        this.encendido = false;
     }
 
     @Override
     public boolean isOn() {
         // TODO Auto-generated method stub
-        return false;
+        return encendido;
     }
 
     @Override
     public void setFrequence(String freq) throws Exception {
         // TODO Auto-generated method stub
-        
+        if(freq.equalsIgnoreCase("AM") || freq.equalsIgnoreCase("FM")){
+            this.frequence = freq.toUpperCase();
+        }
     }
 
     @Override
     public String getFrequence() {
         // TODO Auto-generated method stub
-        return null;
+        return frequence;
     }
 
     @Override
     public void Forward() {
         // TODO Auto-generated method stub
-        
+        if (this.getFrequence() == "AM" || this.getAMActualStation() < 1610){
+            aMActualStation = aMActualStation + 10;
+        }
+        else if (this.getFrequence() == "AM" || this.getAMActualStation() == 1610){
+            aMActualStation = 530;
+        }
+        else if (this.getFrequence() == "FM" || this.getFMActualStation() < 107.9){
+            fMActualStation = fMActualStation + 0.2;
+        }
+        else if (this.getFrequence() == "FM" || this.getFMActualStation() == 107.9){
+            fMActualStation = 87.9;
+        } 
     }
 
     @Override
     public void Backward() {
         // TODO Auto-generated method stub
+        if (this.getFrequence() == "AM" || this.getAMActualStation() > 530){
+            aMActualStation = aMActualStation - 10;
+        }
+        else if (this.getFrequence() == "AM" || this.getAMActualStation() == 530){
+            aMActualStation = 1610;
+        }
+        else if (this.getFrequence() == "FM" || this.getFMActualStation() > 87.9){
+            fMActualStation = fMActualStation - 0.2;
+        }
+        else if (this.getFrequence() == "FM" || this.getFMActualStation() == 87.9){
+            fMActualStation = 107.9;
+        } 
         
     }
 
     @Override
     public double getFMActualStation() {
         // TODO Auto-generated method stub
-        return 0;
+        return this.fMActualStation;
     }
 
     @Override
     public int getAMActualStation() {
         // TODO Auto-generated method stub
-        return 0;
+        return this.aMActualStation;
     }
 
     @Override
     public void setFMActualStation(double actualStation) {
         // TODO Auto-generated method stub
+        this.fMActualStation = actualStation;
         
     }
 
     @Override
     public void setAMActualStation(int actualStation) {
         // TODO Auto-generated method stub
-        
+        this.aMActualStation = actualStation;
     }
 
     @Override
     public void saveFMStation(double actualStation, int slot) {
         // TODO Auto-generated method stub
-        
+        this.slotsFM[slot] = actualStation;
     }
 
     @Override
     public void saveAMStation(int actualStation, int slot) {
         // TODO Auto-generated method stub
-        
+        this.slotsAM[slot] = actualStation;
     }
 
     @Override
     public double getFMSlot(int slot) {
         // TODO Auto-generated method stub
-        return 0;
+        return slotsFM[slot];
     }
 
     @Override
     public int getAMSlot(int slot) {
         // TODO Auto-generated method stub
-        return 0;
+        return slotsAM[slot];
     }
     
 }
